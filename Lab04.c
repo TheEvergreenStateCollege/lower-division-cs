cae9e08a2e509ef33c587fe84d6a9e6b2990347c
D-J-Mesa <167125699+D-J-Mesa@users.noreply.github.com>
2024-04-29 11:58:34 -0700

    Create Lab04.c

---
+++ Lab04.c
@@ -0,0 +1,10 @@
+  int add(int a, int b) {
+      	return a + b;
+      }
+      
+      int main() {
+      	int val;
+        val = add(3, 5);
+        printf("%d\n", val);
+        return 0;
+      }
