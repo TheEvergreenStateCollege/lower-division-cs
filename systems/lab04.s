5dc7363dd4b4ceae50e6677692f540bca90cb754
Studbug <nskkid@gmail.com>
2024-04-29 11:49:10 -0700

    Initial Commit, added lab04.c per lab instructions on canvas.

---
+++ systems/lab04.s
@@ -0,0 +1,56 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 14, 0	sdk_version 14, 4
+	.globl	_add                            ; -- Begin function add
+	.p2align	2
+_add:                                   ; @add
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	str	w0, [sp, #12]
+	str	w1, [sp, #8]
+	ldr	w8, [sp, #12]
+	ldr	w9, [sp, #8]
+	add	w0, w8, w9
+	add	sp, sp, #16
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #48
+	.cfi_def_cfa_offset 48
+	stp	x29, x30, [sp, #32]             ; 16-byte Folded Spill
+	add	x29, sp, #32
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	mov	w8, #0
+	stur	w8, [x29, #-12]                 ; 4-byte Folded Spill
+	stur	wzr, [x29, #-4]
+	mov	w0, #4
+	mov	w1, #6
+	bl	_add
+	stur	w0, [x29, #-8]
+	ldur	w9, [x29, #-8]
+                                        ; implicit-def: $x8
+	mov	x8, x9
+	mov	x9, sp
+	str	x8, [x9]
+	adrp	x0, l_.str@PAGE
+	add	x0, x0, l_.str@PAGEOFF
+	bl	_printf
+	ldur	w0, [x29, #-12]                 ; 4-byte Folded Reload
+	ldp	x29, x30, [sp, #32]             ; 16-byte Folded Reload
+	add	sp, sp, #48
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.section	__TEXT,__cstring,cstring_literals
+l_.str:                                 ; @.str
+	.asciz	"%d\n"
+
+.subsections_via_symbols
