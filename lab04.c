6df3c0e7de2e62275af6d5180f4ac4d462d97f18
Daniel Monroe <dtmonroe95@gmail.com>
2024-04-30 00:05:00 +0000

    lab 04

---
+++ lab04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int add(int a, int b) {
+      	return a + b;
+      }
+      
+      int main() {
+      	int val;
+        val = add(3, 5);
+        printf("%d\n", val);
+        return 0;
+      }
