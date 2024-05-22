18be4baade4a0f0c522b663e65bffee8cc52e901
Studbug <nskkid@gmail.com>
2024-05-22 11:59:23 -0700

    Lab05 Complete program, added functionality to output run time.

--- systems/cache.c
+++ systems/cache.c
@@ -1,17 +1,20 @@
 #include <sys/time.h>
+#include <stdio.h>
 
-      void loop(int a[], int size) {
-      	for (i=0; i&lt;size; i++) {
+    void loop(int a[], long size) {
+        for (int i = 0; i < size; i++) {
            a[i] = i;
         }
-      }
+    }
       
-      int main() {
-      	int size = 100000;
+    int main() {
+      	int size = 999999;
+        int array[size];
         struct timeval tstart, tend;
         gettimeofday(&tstart, NULL);
-        loop
+        loop(array, size);
         gettimeofday(&tend, NULL);
-        //print the time difference
+        long elapsed = (tend.tv_sec - tstart.tv_sec) * 1000000 + tend.tv_usec-tstart.tv_usec;
+        printf("%ld microseconds\n", elapsed);
         return 0;
       }
